85a81f0496670be3aeb680800c3011f8873a70a2
Takeshi Iwamoto <rock.book.ganpon@outlook.com>
2016-01-24 21:42:56 +0900

    ModelとViewModelを作ったところ

---
+++ uniApp1/Models/CommandModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uniApp1.Models
+{
+  public class CommandModel : Common.BindableBase
+  {
+    public static CommandModel Instance { get; } = new CommandModel();
+
+    public void RetweetToast()
+    {
+
+
+    }
+
+  }
+}
---
+++ uniApp1/ViewModels/CommandViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uniApp1.ViewModels
+{
+  public class CommandViewModel : Common.BindableBase
+  {
+    private Models.CommandModel Model { get; } = Models.CommandModel.Instance;
+
+
+
+  }
+}
